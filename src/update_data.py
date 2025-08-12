89bc04cb38f0fae2a1d7cc416ec0ff80ce3aaf0e
unknown <Mehul Bose>
2025-08-13 03:29:11 +0530

    Third Commit

---
+++ src/update_data.py
@@ -0,0 +1,23 @@
+import sys
+import json
+from utils import gen_html
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python gen_html.py <JSON configuration>")
+        sys.exit(1)
+
+    json_config = sys.argv[1]
+
+    with open(json_config, 'r') as f:
+        config = json.load(f)
+    
+    for config_item in config['configs_to_pick']:
+        with open(config_item['JSON'], 'r') as f:
+            print ("Reading configuration for league:", config_item['League'])
+            league_config = json.load(f)
+            gen_html.generate_html(league_config)
+            
+            # TODO: Update Driver Categorization since league data is being updated.
+
+main()
---
+++ src/utils/gen_html.py
@@ -0,0 +1,36 @@
+import sys
+import json
+
+def generate_html(config):
+    print (f"Generating HTML: {config['output_file']}")
+    outfile = open(config['output_file'], 'w')
+
+    # Write the HTML header
+    outfile.write("<html><body>\n")
+    outfile.write(f"<h1>{config['title']}</h1>\n")
+
+    # Write each section
+    for section in config['sections']:
+        outfile.write(f"<h{section['header_level']}>{section['header']}</h{section['header_level']}>\n")
+        if 'content' in section:
+            outfile.write(f"<p>{section['content']}</p>\n")
+        if 'file' in section:
+            try:
+                tablefile = open(section['file'], 'r')
+            except FileNotFoundError:
+                outfile.write("<p>Error: Data Not Found.</p>\n")
+            else:
+                outfile.write("<table>\n")
+                header = True
+                for line in tablefile:
+                    if header:
+                        outfile.write(f"<tr><th>{'</th><th>'.join(line.strip().split(','))}</th></tr>\n")
+                        header = False
+                    else:
+                        outfile.write(f"<tr><td>{'</td><td>'.join(line.strip().split(','))}</td></tr>\n")
+                outfile.write("</table>\n")
+                tablefile.close()
+
+    # Write the HTML footer
+    outfile.write("</body></html>\n")
+    outfile.close()
